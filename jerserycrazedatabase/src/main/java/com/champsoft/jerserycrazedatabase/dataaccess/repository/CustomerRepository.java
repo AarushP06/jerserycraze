5855816b23bbc1e74921b076db290addad47d4eb
liaut <liautaudryan@gmail.com>
2025-11-12 09:51:27 -0500

    did customer repository

--- jerserycrazedatabase/src/main/java/com/champsoft/jerserycrazedatabase/dataaccess/repository/CustomerRepository.java
+++ jerserycrazedatabase/src/main/java/com/champsoft/jerserycrazedatabase/dataaccess/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.champsoft.jerserycrazedatabase.dataaccess.repository;
 
 import com.champsoft.jerserycrazedatabase.dataaccess.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
+
+    List<Customer> findByCustomerId(Long customerId);}
